b6466ed25d750bdf0deecaaba00c7fde28b43b4a
ryan <dorrill@hawaii.edu>
2018-01-18 17:54:35 -1000

    Update and rename ry_perceptron.py to ryCeptron.py
    It works. And plots! I'm not proud of the complete lack of comments and documentation, but here it is.

--- HW1/ryCeptron.py
+++ HW1/ryCeptron.py
@@ -1,5 +1,7 @@
 #import sys
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import matplotlib.transforms as mtransforms
 from matplotlib.dates import HourLocator, MinuteLocator, DateFormatter
 import datetime
 import numpy as np
@@ -16,7 +18,7 @@ def generate_output(data_point,weights):	#Our main perceptron decision function
 	if sum > 0:
 		return 1
 	else:
-		return -1
+		return 0
 
 #Our desired linear separation: y = 3x - 1  -->  
 # If y - 3x + 1 > 0 ---> 1 (above our line)
@@ -26,11 +28,37 @@ b = -1		#intercept
 def generate_training_output(t_data):	#takes training data of x's, y's -> x = t[0][i], y = t[1][i]
 	trained_outputs = []
 	for i in range(len(t_data[0])):
-		if t_data[1][i] > ((m*t_data[0][i]) + b):
+		if t_data[1][i] - ((m*t_data[0][i]) + b)> 0:
 			trained_outputs.append(1)
 		else:
-			trained_outputs.append(-1)
+			trained_outputs.append(0)
 	return trained_outputs
+
+def plot_stuff(trained_weights,training_data):
+	x1=-10
+	y1=(m*x1)+b
+	x2=10
+	y2=(m*x2)+b
+	plt.figure()
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.title('Learning Linearly Separable Data')
+	plt.axis([-20, 20, -20, 20])
+	plt.plot([x1, x2], [y1, y2], 'k-', color = 'r',label='Correct line')
+
+	m_l = -1*trained_weights[1]/(trained_weights[2])
+	b_l = -1*trained_weights[0]/(trained_weights[2])
+	y1_learned = (b_l+(m_l*x1))
+	y2_learned = (b_l+(m_l*x2))
+	plt.plot([x1, x2], [y1_learned, y2_learned], 'k-', color = 'green',label='Learned line')
+	for i in range(len(training_data[0])):
+		if training_data[1][i] - ((m*training_data[0][i]) + b)> 0:
+			plt.scatter(training_data[0][i],training_data[1][i],c='r')
+		else:
+			plt.scatter(training_data[0][i],training_data[1][i],c='b')
+	plt.legend(loc=1)
+	plt.show()
+
 
 
 def update_weights(data,trained_outputs,weights,l_rate):	#takes training data and returns training outputs	
@@ -39,24 +67,31 @@ def update_weights(data,trained_outputs,weights,l_rate):	#takes training data an
 		dat_point[1] = data[0][i]	#take ith x value
 		dat_point[2] = data[1][i]	#take ith y value
 		output = generate_output(dat_point,weights)
-		print "output: ",output,' ',trained_outputs[i]
+		#print "output: ",output,' ',trained_outputs[i],"    diff: ",trained_outputs[i]-output
 		if output != trained_outputs[i]:
 			for j in range(len(weights)):
-				print "weight",j, " = ",weights[j], " + ",l_rate,"*",output,"*",dat_point[j]
-				weights[j] = weights[j] + (l_rate*output)*dat_point[j]
+				#print "weight",j, " = ",weights[j], " + ",l_rate,"*(",trained_outputs[i],' - ',-1*output,")*",dat_point[j]
+				weights[j] = weights[j] + (l_rate)*(trained_outputs[i]-output)*dat_point[j]
 	return weights
 
-data_length = 10000	#how many data points to create
-learning_rate = 0.1
+data_length = 200	#how many data points to create
+learning_rate = 0.05
 
-t_mu, t_sigma = 1, 1.5 # mean and standard deviation
+t_mu, t_sigma = -1, 7 # mean and standard deviation
 training_x = np.random.normal(t_mu, t_sigma, data_length)
 training_y = np.random.normal(t_mu, t_sigma, data_length)
 training_data = (training_x,training_y)
+
 training_outputs = generate_training_output(training_data)
+
 rand_weights = (np.random.normal(0, 0.05,3)) #generate three training rates -> w_intercept,w_x,w_y
+epochs = 1
 trained_weights = update_weights(training_data,training_outputs,rand_weights,learning_rate)
-print "Trained weights: ",trained_weights
+plot_stuff(trained_weights,training_data)
+for k in range(0,epochs):
+	trained_weights = update_weights(training_data,training_outputs,trained_weights,learning_rate)
+	plot_stuff(trained_weights,training_data)
+	print "Trained weights: ",trained_weights/(trained_weights[2])
 
 
 
